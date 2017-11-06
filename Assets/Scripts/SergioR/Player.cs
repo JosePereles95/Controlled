403f21931b7bc48f217aa4485c18c3c5ae1cc67c
makastreteam <sergiomacastre@gmail.com>
2017-11-06 10:37:39 +0100

    06/11

--- Assets/Scripts/SergioR/Player.cs
+++ Assets/Scripts/SergioR/Player.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using UnityEngine;
 
 [RequireComponent(typeof(Controller2D))]
@@ -139,58 +138,5 @@ public class Player : MonoBehaviour
         float targetVelocityX = directionalInput.x * moveSpeed;
         velocity.x = Mathf.SmoothDamp(velocity.x, targetVelocityX, ref velocityXSmoothing, (controller.collisions.below ? accelerationTimeGrounded : accelerationTimeAirborne));
         velocity.y += gravity * Time.deltaTime;
-=======
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[RequireComponent (typeof (Controller2D))]
-
-public class Player : MonoBehaviour {
-
-    public float jumpHeight = 4;
-    public float timeToJumpApex = .4f;
-
-    public float accelerationTimeAirbone = .2f;
-    public float accelerationTimeGrounded = .1f;
-
-    float moveSpeed = 6;
-    float gravity;
-
-    float jumpVelocity;
-    Vector3 velocity;
-
-    float velocityXSmoothing;
-
-    Controller2D controller;
-
-	void Start ()
-    {
-        controller = GetComponent<Controller2D>();
-
-        gravity = -(2 * jumpHeight) / Mathf.Pow(timeToJumpApex, 2);
-        jumpVelocity = Mathf.Abs(gravity) * timeToJumpApex;
-        print("Gravity: " + gravity + " Jump velocity: " + jumpVelocity);
-	}
-	
-    void Update()
-    {
-        if(controller.collisions.above || controller.collisions.below)
-        {
-            velocity.y = 0;
-        }
-
-        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-
-        if(Input.GetKeyDown(KeyCode.Space) && controller.collisions.below)
-        {
-            velocity.y = jumpVelocity;
-        }
-
-        float targetVelocityX = input.x * moveSpeed;
-        velocity.x = Mathf.SmoothDamp(velocity.x, targetVelocityX, ref velocityXSmoothing, (controller.collisions.below)?accelerationTimeGrounded:accelerationTimeAirbone);
-        velocity.y += gravity * Time.deltaTime;
-        controller.Move(velocity * Time.deltaTime);
->>>>>>> SergioL
     }
 }
