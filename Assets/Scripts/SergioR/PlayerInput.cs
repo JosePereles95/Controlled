38a407aa90f4224dc89542439c0dee979736edd8
Vicent Santamarta <vicent.santamarta@gmail.com>
2017-11-13 12:19:54 +0100

    forma basica de parasitar hecho
    Hay cosas por mejorar, pero la base de parasitar ya esta hecha

--- Assets/Scripts/SergioR/PlayerInput.cs
+++ Assets/Scripts/SergioR/PlayerInput.cs
@@ -4,15 +4,44 @@
 public class PlayerInput : MonoBehaviour
 {
     private Player player;
+    private string playerState;
+    private Player controlledTripulant;
+    public GameObject vujBody;
+    public GameObject canControlFlag;
 
     private void Start()
     {
         player = GetComponent<Player>();
+        playerState = "NotControlling";
+        canControlFlag.SetActive(false);
     }
 
     private void Update()
     {
-        Vector2 directionalInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        switch (playerState)
+        {
+            case "CanControl":
+                if(Input.GetKeyDown(KeyCode.E)) Parasitar();
+
+                else ControlVUJ();
+                break;
+            case "NotControlling":
+                ControlVUJ();
+                break;
+            case "Controlling":
+                if (Input.GetKeyDown(KeyCode.E)) Desparasitar();
+                else
+                {
+                    ControlTripulant();
+                    transform.position = controlledTripulant.transform.position;
+                }
+                break;
+        }
+    }
+
+    private void ControlVUJ()
+    {
+         Vector2 directionalInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         player.SetDirectionalInput(directionalInput);
 
         if (Input.GetButtonDown("Jump"))
@@ -23,6 +52,60 @@ public class PlayerInput : MonoBehaviour
         if (Input.GetButtonUp("Jump"))
         {
             player.OnJumpInputUp();
+        }
+    }
+
+    private void ControlTripulant()
+    {
+        Vector2 directionalInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        controlledTripulant.SetDirectionalInput(directionalInput);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            controlledTripulant.OnJumpInputDown();
+        }
+
+        if (Input.GetButtonUp("Jump"))
+        {
+            controlledTripulant.OnJumpInputUp();
+        }
+    }
+
+    private void Parasitar()
+    {
+        if (canControlFlag.activeInHierarchy == true) canControlFlag.SetActive(false);
+
+        player.enabled = false;
+        vujBody.SetActive(false);
+        playerState = "Controlling";
+    }
+
+    private void Desparasitar()
+    {
+        controlledTripulant.SetDirectionalInput(new Vector2(0, 0));
+        player.enabled = true;
+        vujBody.SetActive(true);
+        playerState = "NotControlling";
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "TripB" && playerState == "NotControlling")
+        {
+            controlledTripulant = other.gameObject.GetComponent<Player>();
+            canControlFlag.SetActive(true);
+            playerState = "CanControl";
+        }
+
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+       if(other.tag == "TripB" && playerState == "CanControl")
+        {
+            canControlFlag.SetActive(false);
+            controlledTripulant = null;
+            playerState = "NotControlling";
         }
     }
 }
