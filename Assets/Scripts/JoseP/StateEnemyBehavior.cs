b8e671afea9f6e3147085f111c4ad9bd3021711f
makastreteam <sergiomacastre@gmail.com>
2017-11-14 18:04:39 +0100

    pull Master

---
+++ Assets/Scripts/JoseP/StateEnemyBehavior.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class StateEnemyBehavior : MonoBehaviour {
+	
+	public Transform[] wayPoints;
+
+	[HideInInspector] public IEnemyState currentState;
+	[HideInInspector] public PatrolState patrolState;
+
+	public static StateEnemyBehavior Instance;
+
+	void Awake(){
+		patrolState = new PatrolState (this);
+	}
+
+	void Start (){
+		currentState = patrolState;
+	}
+
+	void Update (){
+		currentState.UpdateState ();
+	}
+}
