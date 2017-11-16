f53473b0e3b19991b7e8956ec6e2c4ac5d9cefa6
makastreteam <sergiomacastre@gmail.com>
2017-11-16 20:16:57 +0100

    laser

--- Assets/Scripts/SergioR/laserRojo.cs
+++ Assets/Scripts/SergioR/laserRojo.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 
 public class laserRojo : MonoBehaviour {
 
-    private LineRenderer lr;
+    /*private LineRenderer lr;
 
 	// Use this for initialization
 	void Start ()
@@ -30,6 +30,40 @@ public class laserRojo : MonoBehaviour {
         {
             lr.SetPosition(1, new Vector3(20, 0, 0));
         }
-	}
+	}*/
+
+    /*private LineRenderer lineRenderer;
+    //public Transform laserHit;
+
+    void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        //lineRenderer.enabled = false;
+        //lineRenderer.useWorldSpace = true;
+    }
+
+    void Update()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, -transform.up);
+        //Debug.DrawLine(transform.position, hit.point);
+        //laserHit.position = -hit.point;
+    }*/
+
+    void Update()
+    {
+        //If the left mouse button is clicked.
+        if (Input.GetMouseButtonDown(0))
+        {
+            //Get the mouse position on the screen and send a raycast into the game world from that position.
+            Vector2 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(worldPoint, Vector2.zero);
+
+            //If something was hit, the RaycastHit2D.collider will not be null.
+            if (hit.collider != null)
+            {
+                Debug.Log(hit.collider.name);
+            }
+        }
+    }
 
 }
