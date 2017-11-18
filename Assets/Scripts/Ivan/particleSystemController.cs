91aa52c2761ef407ef50e68a414db7a95cdc2c2c
SergioLosan <al288149@uji.es>
2017-11-18 19:59:00 +0100

    Sprint2

---
+++ Assets/Scripts/Ivan/particleSystemController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class particleSystemController : MonoBehaviour {
+    private ParticleSystem pS;
+    
+	// Use this for initialization
+	void Start () {
+        pS = GetComponent<ParticleSystem>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown("v")) {
+            if (pS.isEmitting)
+            {
+                pS.Stop();
+                Debug.Log("Stopping particle system");
+            }
+            else {
+                pS.Play();
+                Debug.Log("Playing particle system");
+            }
+        }
+	}
+}
