fdc96487193f8e96e7b643bc3e25453368a02a37
Ivanlol97 <33065563+Ivanlol97@users.noreply.github.com>
2017-11-19 17:37:34 +0100

    CASI PARA VOMIT
    Falta que el vomito sea limitado y que pare a los enemigos

--- Assets/Scripts/Ivan/particleSystemController.cs
+++ Assets/Scripts/Ivan/particleSystemController.cs
@@ -4,23 +4,26 @@ using UnityEngine;
 
 public class particleSystemController : MonoBehaviour {
     private ParticleSystem pS;
+ //   private Animation anim;
     
 	// Use this for initialization
 	void Start () {
         pS = GetComponent<ParticleSystem>();
-	}
+    }
 	
 	// Update is called once per frame
 	void Update () {
 
-        if (Input.GetKeyDown("v")) {
+        if (Input.GetKeyDown(KeyCode.V)) { 
             if (pS.isEmitting)
             {
                 pS.Stop();
+             //   anim.Play("idle");
                 Debug.Log("Stopping particle system");
             }
             else {
                 pS.Play();
+             //   anim.Play("Vomit");
                 Debug.Log("Playing particle system");
             }
         }
--- Assets/Scripts/Ivan/vomitController.cs
+++ Assets/Scripts/Ivan/vomitController.cs
@@ -4,25 +4,19 @@ using UnityEngine;
 
 public class vomitController : MonoBehaviour {
     public int duration = 2;
-    private Animator anim;
-    //public SkinnedMeshRenderer sr;
     public Player p;
-    public GameObject g;
+    public GameObject[] v;
+    public bool isVomiting;
     private bool vomiting = false;
- // private bool vomitlake = false;
     private bool jumping = false;
     private float minimum = 0f;
     private float maximum = 1f;
- // private int cont;
- // private int cont2;
 
     private float time = 0.0f;
     private float timeVomiting = 0.0f;
 
     // Use this for initialization
     void Start () {
-        anim = GetComponent<Animator>();
-        this.enabled = false;
         jumping = false;
     }
 	
@@ -30,8 +24,7 @@ public class vomitController : MonoBehaviour {
 	void Update () {
         time += Time.deltaTime;
         timeVomiting += Time.deltaTime;
-      //this.transform.position = new Vector2(p.transform.position.x + 5, p.transform.position.y);
-        if (Input.GetKeyDown("Space"))
+        if (Input.GetKeyDown("space"))
         {
             jumping = true;
             time = 0.0f;
@@ -41,82 +34,31 @@ public class vomitController : MonoBehaviour {
         if (time > 2f && !vomiting)
         {
             timeVomiting = 0.0f;
-            anim.Play("idle");
             vomiting = true;
             jumping = false;
-          //this.GetComponentInChildren<SpriteRenderer>().enabled = true;
 
         }
-        if (timeVomiting > 2f && vomiting) {
-            Disappear();
-            Debug.Log("Va a destruirlo");
-            Destroy(g);
-            Debug.Log("Destruido");
+        if (timeVomiting >22f && vomiting) {
+            Debug.Log("Eliminar");
+            this.isVomiting = false;
+            vomiting = false;
         }
 
         if (vomiting && jumping)
         {
             timeVomiting = 0.0f;
             vomiting = false;
-        //  this.enabled = false;
-        //this.GetComponentInChildren<SpriteRenderer>().enabled = false;
         }
 
-     /* if (vomiting)
-            Appear();
-        else if (jumping || !vomiting)
-            Disappear();*/
-
-        if (Input.GetKeyDown("v") && !jumping)
+        if (!isVomiting && Input.GetKeyDown(KeyCode.V) && !jumping)
         {
-            anim.enabled = true;
-            Debug.Log("Va a vomitar");
-            anim.Play("Vomit");
             Debug.Log("Vomitaaa");
-            /* this.enabled = true;
-               this.transform.position = new Vector2(p.transform.position.x + 5, p.transform.position.y);
-               this.GetComponentInChildren<SpriteRenderer>().enabled = true;*/
-            g = Instantiate(g);
-            g.transform.position = new Vector2(p.transform.position.x + 5, p.transform.position.y);
+            GameObject clon = Instantiate(v[0], transform.position, transform.rotation) as GameObject;
+            this.isVomiting = true;
+            clon.transform.position = new Vector2(p.transform.position.x + 5, p.transform.position.y);
+            Destroy(clon, 10);
             Debug.Log("Creado charco");
         }
-
-        
-        /*if (cont == 60 && vomiting) {
-              anim.Play("idle");
-              vomiting = false;
-              sr.
-              sr.enabled = true;
-            //sr.SetActive(true);
-            vomitlake = true;
-          }
-          if (vomiting) {
-              cont += 1;
-          }
-          if (vomitlake) {
-              cont2 += 1;
-          }
-         if (cont2 == 5) {
-              //sr.enabled = false;
-           sr.SetActive(false);
-          }*/
-
-    }
-    void Appear()
-    {
-        this.GetComponent<SkinnedMeshRenderer>().enabled = true;
-        
-        this.GetComponentInChildren<SpriteRenderer>().color = new Color(1f, 1f, 1f, Mathf.SmoothStep(minimum, maximum, timeVomiting / duration));
-        this.enabled = true;
-    }
-
-    void Disappear()
-    {
-       
-      this.GetComponent<SkinnedMeshRenderer>().enabled = false;
-
-      this.GetComponentInChildren<SpriteRenderer>().color = new Color(1f, 1f, 1f, Mathf.SmoothStep(maximum, minimum, timeVomiting / duration));
-        this.enabled = false;
     }
     
 }
