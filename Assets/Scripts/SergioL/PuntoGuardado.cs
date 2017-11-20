1d496cf49b9968c870efb940ce9acecf4ec76566
SergioLosan <al288149@uji.es>
2017-11-20 23:06:45 +0100

    Puntos_Guardado

---
+++ Assets/Scripts/SergioL/PuntoGuardado.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuntoGuardado : MonoBehaviour {
+
+    public Sistema_Jueg levelManager;
+
+    // Use this for initialization
+    void Start()
+    {
+        levelManager = FindObjectOfType<Sistema_Jueg>();
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.name == "Player")
+        {
+            levelManager.Checkpoint_Actual = gameObject;
+        }
+
+    }
+}
