fb48a4a216e57c2ac434286fac3eb30387e720d4
Carlos Tello <al341537@uji.es>
2017-11-21 21:12:18 +0100

    Inicio del sprite 4
    Inicio del sprite 4

---
+++ Assets/Scripts/JoseP/SightTrigger.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SightTrigger : MonoBehaviour {
+
+	void OnTriggerStay2D(Collider2D other) {
+		gameObject.GetComponentInParent<StateEnemyBehavior> ().SightTriggered (other);
+	}
+
+	void OnTriggerExit2D(Collider2D other) {
+		gameObject.GetComponentInParent<StateEnemyBehavior> ().SightExit (other);
+	}
+}
---
+++ Assets/Scripts/SergioR/laserRojo.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (LineRenderer))]
+
+public class laserRojo : MonoBehaviour {
+
+    private LineRenderer lr;
+
+	// Use this for initialization
+	void Start ()
+    {
+        lr = GetComponent<LineRenderer>();
+	}
+
+    // Update is called once per frame
+    void Update ()
+    {
+        RaycastHit hit;
+
+        if(Physics.Raycast(transform.position, transform.forward, out hit))
+        {
+            if(hit.collider)
+            {
+                lr.SetPosition(1, new Vector3(hit.distance, 0, 0));
+            }
+        }
+        else
+        {
+            lr.SetPosition(1, new Vector3(20, 0, 0));
+        }
+	}
+
+}
