0df111cdd331a901b575cd05d9111ba0392ebc70
Chepriav <al341533@uji.es>
2017-11-21 22:14:03 +0100

    Sprint 3

--- Assets/Scripts/JoseP/StateEnemyBehavior.cs
+++ Assets/Scripts/JoseP/StateEnemyBehavior.cs
@@ -5,14 +5,19 @@ using System.Collections.Generic;
 public class StateEnemyBehavior : MonoBehaviour {
 	
 	public Transform[] wayPoints;
+	[HideInInspector] public Vector3 posPlayer;
+	[HideInInspector] public Transform target;
+	[HideInInspector] public int moveSpeed = 4;
 
 	[HideInInspector] public IEnemyState currentState;
 	[HideInInspector] public PatrolState patrolState;
+	[HideInInspector] public ChaseState chaseState;
 
 	public static StateEnemyBehavior Instance;
 
 	void Awake(){
 		patrolState = new PatrolState (this);
+		chaseState = new ChaseState (this);
 	}
 
 	void Start (){
@@ -21,5 +26,19 @@ public class StateEnemyBehavior : MonoBehaviour {
 
 	void Update (){
 		currentState.UpdateState ();
+		posPlayer = this.transform.position;
+	}
+
+	public void SightTriggered(Collider2D other) {
+		if (other.tag == "Player") {
+			target = other.transform;
+			currentState.ToChaseState();
+		}
+	}
+
+	public void SightExit(Collider2D other) {
+		if (other.tag == "Player") {;
+			currentState.ToPatrolState();
+		}
 	}
 }
