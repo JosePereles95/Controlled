d48ed75342c7151337bcb951b9815ae908eba758
Vicent Santamarta <vicent.santamarta@gmail.com>
2017-11-21 22:56:46 +0100

    Recarga para el sprint 4

--- Assets/Scripts/Ivan/particleSystemController.cs
+++ Assets/Scripts/Ivan/particleSystemController.cs
@@ -4,23 +4,27 @@ using UnityEngine;
 
 public class particleSystemController : MonoBehaviour {
     private ParticleSystem pS;
+    private Animator anim;
     
 	// Use this for initialization
 	void Start () {
         pS = GetComponent<ParticleSystem>();
+        anim = GetComponentInParent<Animator>();
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-
-        if (Input.GetKeyDown("v")) {
+        if (Input.GetKeyDown(KeyCode.V)) { 
+			Debug.Log(anim.name);
             if (pS.isEmitting)
             {
                 pS.Stop();
+                anim.Play("Idle");
                 Debug.Log("Stopping particle system");
             }
             else {
                 pS.Play();
+                anim.Play("Vomit");
                 Debug.Log("Playing particle system");
             }
         }
