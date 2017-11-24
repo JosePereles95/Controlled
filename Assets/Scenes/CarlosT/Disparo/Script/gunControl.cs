44cde00b12f1f93e86ca889dd51acd57e26091db
Carlos Tello <al341537@uji.es>
2017-11-24 11:33:08 +0100

    RotacionBrazo1

---
+++ Assets/Scenes/CarlosT/Disparo/Script/gunControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gunControl : MonoBehaviour {
+    Rigidbody2D rb;
+    Vector2 dir;
+    float angle;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        dir = Input.mousePosition - transform.position;
+        angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        rb.MoveRotation(angle);
+	}
+}
