9a925b12361d06adf552b64cb2312694c027490d
SergioLosan <al288149@uji.es>
2017-11-25 19:11:30 +0100

    Añadir

---
+++ Assets/Scenes/SergioL/IvanD/Tutorial/LimitesTutorial.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LimitesTutorial : MonoBehaviour {
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "AlienSalvaje")
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
---
+++ Assets/Scenes/SergioL/IvanD/Tutorial/Tutorial.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tutorial : MonoBehaviour {
+
+    public Renderer rend;
+    public GameObject doctorTuto;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        rend.enabled = false;
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "AlienSalvaje")
+        {
+            rend.enabled = true;
+            persecucionTuto.perseguir = true;
+            doctorTuto.SetActive(true);
+        }
+    }
+}
