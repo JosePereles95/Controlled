3451f3cfa3833cb53e9ffc1c48a6bc5b32f9b790
SergioLosan <al288149@uji.es>
2017-11-25 21:46:21 +0100

    Estructura_Nivel

--- Assets/Scripts/Ivan/particleSystemController.cs
+++ Assets/Scripts/Ivan/particleSystemController.cs
@@ -4,23 +4,29 @@ using UnityEngine;
 
 public class particleSystemController : MonoBehaviour {
     private ParticleSystem pS;
-    
+    private Animator anim;
+    public Material newMat;
 	// Use this for initialization
 	void Start () {
         pS = GetComponent<ParticleSystem>();
-	}
-	
+        anim = GetComponentInParent<Animator>();
+        pS.GetComponent< ParticleSystemRenderer > ().material = newMat;
+
+    }
+
 	// Update is called once per frame
 	void Update () {
-
-        if (Input.GetKeyDown("v")) {
+        if (Input.GetKeyDown(KeyCode.V)) { 
+			Debug.Log(anim.name);
             if (pS.isEmitting)
             {
                 pS.Stop();
+                anim.Play("Idle");
                 Debug.Log("Stopping particle system");
             }
             else {
                 pS.Play();
+                anim.Play("Vomit");
                 Debug.Log("Playing particle system");
             }
         }
--- Assets/Scripts/JoseP/PatrolState.cs
+++ Assets/Scripts/JoseP/PatrolState.cs
@@ -6,16 +6,30 @@ public class PatrolState : IEnemyState {
 	StateEnemyBehavior enemy;
 	int nextWayPoint = 0;
 
-	public Transform target;
-	public int moveSpeed = 4;
 	public int rotationSpeed = 0;
+
+	private bool facingRight = false;
+	
+    private float time = 0.0f;
+    private bool stopping = false;
 
 	public PatrolState (StateEnemyBehavior enemy) {
 		this.enemy = enemy;
 	}
 
 	public void UpdateState () {
-		target = enemy.wayPoints [nextWayPoint];
+        if (stopping)
+        {
+            time += Time.deltaTime;
+            if (time > 22f)
+            {
+               enemy.moveSpeed = 4;
+                stopping = false;
+            }
+        }
+        else { enemy.moveSpeed = 4; }
+
+		enemy.target = enemy.wayPoints [nextWayPoint];
 
 		Patrol ();
 	}
@@ -24,25 +38,70 @@ public class PatrolState : IEnemyState {
 		//Cant change to same state
 	}
 
-	void Patrol() {
+	public void ToChaseState() {
+		enemy.currentState = enemy.chaseState;
+	}
+
+	private void Patrol() {
 		
-		Vector3 dir = target.position - enemy.transform.position;
+		Vector3 dir = enemy.target.position - enemy.transform.position;
 		dir.z = 0.0f;
 
 		if (dir != Vector3.zero) {
-			
 			enemy.transform.rotation = Quaternion.Slerp (enemy.transform.rotation, 
 				Quaternion.FromToRotation (Vector3.right, dir), rotationSpeed * Time.deltaTime);
 		}
 
-		enemy.transform.position += (target.position - enemy.transform.position).normalized * moveSpeed * Time.deltaTime;
-		Debug.Log ("MOVING");
+		enemy.transform.position += (enemy.target.position - enemy.transform.position).normalized * enemy.moveSpeed * Time.deltaTime;
+
+		if (((enemy.target.position - enemy.transform.position).normalized).x > 0)
+			FlipDroide (1);
+		else
+			FlipDroide (-1);
+
 		if (Vector3.Distance (enemy.transform.position, enemy.wayPoints [nextWayPoint].position) < 1f) {
-			Debug.Log ("Change");
 			if (nextWayPoint < enemy.wayPoints.Length - 1)
 				nextWayPoint++;
 			else
 				nextWayPoint = 0;
 		}
 	}
+
+	private void FlipDroide(float horizontal)
+	{
+		if (horizontal > 0 && !facingRight || horizontal < 0 && facingRight)
+		{
+			facingRight = !facingRight;
+
+			Vector3 theScale = enemy.transform.localScale;
+			float thePosition = enemy.transform.localPosition.x;
+
+			enemy.transform.localPosition = new Vector3(thePosition, enemy.transform.localPosition.y, enemy.transform.localPosition.z);
+
+			theScale.x *= -1;
+			enemy.transform.localScale = theScale;
+		}
+	}
+    private void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "vomit")
+        {
+            Debug.Log("TRIGGERED");
+            /*enemy.moveSpeed = 0;
+            stopping = true;
+            time = 0.0f;*/
+            enemy.moveSpeed = 0;
+            new WaitForSecondsVomit();
+            enemy.moveSpeed = 4;
+            //StartCoroutine(TimeLapse());
+            /* Vector3 velocity;
+                if (!this.rigidbody2D.isSleeping){
+                    this.rigidbody.sleep();
+                    this.velocity = this.rigidbody.velocity;
+                } else {
+                    this.rigidbody.WakeUp();
+                    this.rigidbody.velocity = this.velocity;
+                }*/
+        }
+    }
 }
