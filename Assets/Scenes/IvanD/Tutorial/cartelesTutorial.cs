2b0a06e8b029f9410cb7c6a951526735f3e316a0
makastreteam <sergiomacastre@gmail.com>
2017-11-27 22:54:50 +0100

    merge con rama de jose

---
+++ Assets/Scenes/IvanD/Tutorial/cartelesTutorial.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cartelesTutorial : MonoBehaviour {
+
+    public Renderer rend;
+    bool soloUnaVez = true;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        rend.enabled = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player" && soloUnaVez == true)
+        {
+            rend.enabled = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            rend.enabled = false;
+            soloUnaVez = false;
+        }
+    }
+}
---
+++ Assets/Scripts/Ivan/vomitController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class vomitController : MonoBehaviour {
+    public int duration = 2;
+    public Player p;
+    public GameObject[] v;
+    public bool isVomiting;
+    private bool vomiting = false;
+    private bool jumping = false;
+    private float minimum = 0f;
+    private float maximum = 1f;
+
+    private float time = 0.0f;
+    private float timeVomiting = 0.0f;
+
+    // Use this for initialization
+    void Start () {
+        jumping = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        time += Time.deltaTime;
+        timeVomiting += Time.deltaTime;
+        if (Input.GetKeyDown("space"))
+        {
+            jumping = true;
+            time = 0.0f;
+
+        }
+
+        if (time > 2f && !vomiting)
+        {
+            timeVomiting = 0.0f;
+            vomiting = true;
+            jumping = false;
+
+        }
+        if (timeVomiting >22f && vomiting) {
+            Debug.Log("Eliminar");
+            this.isVomiting = false;
+            vomiting = false;
+        }
+
+        if (vomiting && jumping)
+        {
+            timeVomiting = 0.0f;
+            vomiting = false;
+        }
+
+        if (!isVomiting && Input.GetKeyDown(KeyCode.V) && !jumping)
+        {
+            Vomit ();
+        }
+    }
+	
+	public void Vomit(){
+		Debug.Log("Vomitaaa");
+		GameObject clon = Instantiate(v[0], transform.position, transform.rotation) as GameObject;
+		this.isVomiting = true;
+		clon.transform.position = new Vector2(p.transform.position.x + 5, p.transform.position.y);
+		Destroy(clon, 10);
+		Debug.Log("Creado charco");
+	}
+}
