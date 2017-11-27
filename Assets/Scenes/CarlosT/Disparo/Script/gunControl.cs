3bd85d4819c0f020f26763bed44d93cfcea52b6f
Carlos Tello <al341537@uji.es>
2017-11-27 11:55:36 +0100

    Disparo Becari0
    La bala se crea en la posición del brazo y con la rotración adecuada.
    Todavía no se mueve.

--- Assets/Scenes/CarlosT/Disparo/Script/gunControl.cs
+++ Assets/Scenes/CarlosT/Disparo/Script/gunControl.cs
@@ -15,15 +15,38 @@ public class gunControl : MonoBehaviour {
     public float maxAngle;
     public float minAngle;
 
-	// Use this for initialization
-	void Start () {
+    //------------------------Prueba---------------------------------
+    public float fireRate = 0;
+    public float Damage = 10;
+    public LayerMask whatToHit;
+
+    public Transform BulletTrailPrefab;
+
+    float timeToFiere = 0;
+    Transform firePoint;
+    //------------------------Prueba---------------------------------
+
+
+    // Use this for initialization
+    void Start () {
         rb = GetComponent<Rigidbody2D>();
         theController = GetComponentInParent<NpcMovement>();
         //brazo.localEulerAngles = Vector3.forward;
 	}
-	
-	// Update is called once per frame
-	void LateUpdate () {
+
+    //------------------------Prueba---------------------------------
+    void Awake()
+    {
+        firePoint = transform.Find("FirePoint");
+        if (firePoint == null)
+        {
+            Debug.LogError("No firePoint? WHAT?!");
+        }
+    }
+    //------------------------Prueba---------------------------------
+
+    // Update is called once per frame
+    void LateUpdate () {
         Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         dir = mousePos - transform.position;
 
@@ -51,12 +74,45 @@ public class gunControl : MonoBehaviour {
         
         brazo.localEulerAngles = brazo.localEulerAngles + new Vector3(0, 0, lastAngle+90);
 
+        //------------------------Prueba---------------------------------
+        if (fireRate == 0)
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                Shoot();
+            }
+        }
 
+        else
+        {
+            if (Input.GetButtonDown("Fire1") && Time.time > timeToFiere)
+            {
+                timeToFiere = Time.time + 1 / fireRate;
+                Shoot();
+            }
+        }
+        //------------------------Prueba---------------------------------
 
     }
 
-    private void Shoot(float angle)
+    //------------------------Prueba---------------------------------
+    void Shoot()
     {
-        GameObject bullet = cargador.GetBullet();
+        Vector2 mousePosition = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+        Vector2 firePointPosition = new Vector2(firePoint.position.x, firePoint.position.y);
+        RaycastHit2D hit = Physics2D.Raycast(firePointPosition, mousePosition - firePointPosition, 100, whatToHit);
+        Effect();
+        Debug.DrawLine(firePointPosition, (mousePosition - firePointPosition) * 100, Color.cyan);
+        if (hit.collider != null)
+        {
+            Debug.DrawLine(firePointPosition, hit.point, Color.red);
+            Debug.Log("We hit " + hit.collider.name + " an did " + Damage + " damage");
+        }
     }
+
+    void Effect()
+    {
+        Instantiate(BulletTrailPrefab, brazo.position, brazo.rotation);
+    }
+    //------------------------Prueba---------------------------------
 }
