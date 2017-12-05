68f2db86a7b5980952cab2daf69c5d14291ac657
Vicent Santamarta <vicent.santamarta@gmail.com>
2017-12-05 12:47:15 +0100

    alertSignal

--- Assets/Scripts/JoseP/SightTrigger.cs
+++ Assets/Scripts/JoseP/SightTrigger.cs
@@ -2,14 +2,21 @@
 using System.Collections;
 
 public class SightTrigger : MonoBehaviour {
+    public SightAlert theSignal;
 
 	void OnTriggerStay2D(Collider2D other) {
-		if (other.tag == "Player")
-			gameObject.GetComponentInParent<StateEnemyBehavior> ().SightTriggered (other);
-	}
+        if (other.tag == "Player")
+        {
+            gameObject.GetComponentInParent<StateEnemyBehavior>().SightTriggered(other);
+            theSignal.ActivateAlert();
+        }
+    }
 
 	void OnTriggerExit2D(Collider2D other) {
-		if (other.tag == "Player")
-			gameObject.GetComponentInParent<StateEnemyBehavior> ().SightExit (other);
+        if (other.tag == "Player")
+        {
+            gameObject.GetComponentInParent<StateEnemyBehavior>().SightExit(other);
+            theSignal.ActivaLost();
+        }
 	}
 }
---
+++ Assets/Scripts/VicentS/SightAlert.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SightAlert : MonoBehaviour {
+
+    public Sprite alertSprite;
+    public Sprite lostSprite;
+
+    private SpriteRenderer theRenderer;
+
+    private void Start()
+    {
+        theRenderer = GetComponent<SpriteRenderer>();
+        this.gameObject.SetActive(false);
+    }
+
+    public void ActivateAlert()
+    {
+        theRenderer.sprite = alertSprite;
+        this.gameObject.SetActive(true);
+
+        StartCoroutine("DeactivateAlarm");
+    }
+
+    public void ActivaLost()
+    {
+        theRenderer.sprite = alertSprite;
+        this.gameObject.SetActive(true);
+
+        StartCoroutine("DeactivateAlarm");
+    }
+
+    private IEnumerator DeactivateAlert()
+    {
+        yield return new WaitForSeconds(1.5f);
+        this.gameObject.SetActive(false);
+    }
+
+
+}
