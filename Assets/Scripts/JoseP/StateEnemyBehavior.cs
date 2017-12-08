de0eda1ae0f46eed6117ea7b266dc7d1ee1543d7
Chepriav <al341533@uji.es>
2017-12-08 21:01:09 +0100

    Modificacion de escenario

--- Assets/Scripts/JoseP/StateEnemyBehavior.cs
+++ Assets/Scripts/JoseP/StateEnemyBehavior.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 
 [RequireComponent(typeof(NpcMovement))]
 public class StateEnemyBehavior : MonoBehaviour {
-	
+
 	public Transform[] wayPoints;
 	[HideInInspector] public Vector3 posPlayer;
 	[HideInInspector] public Transform target;
@@ -17,21 +17,60 @@ public class StateEnemyBehavior : MonoBehaviour {
 	[HideInInspector] public IEnemyState currentState;
 	[HideInInspector] public PatrolState patrolState;
 	[HideInInspector] public ChaseState chaseState;
-    [HideInInspector] public ControlledState controlledState;
+	[HideInInspector] public ControlledState controlledState;
 	[HideInInspector] public DiedState diedState;
 
-    public static StateEnemyBehavior Instance;
+	public static StateEnemyBehavior Instance;
 
-    private NpcMovement theController;
+	//Life magement
+	public float vidaInicial = 100f;
+	private TankHealth lifeBar;
+
+	private NpcMovement theController;
+
+	//Spawn management
+	public SpawnPoint spawnPoint;
 
 	void Awake(){
-        theController = GetComponent<NpcMovement>();
-		parado = false;
-		patrolState = new PatrolState (this, theController);
-		chaseState = new ChaseState (this, theController);
-        controlledState = new ControlledState(this, theController);
-		diedState = new DiedState(this, theController);
+		InitCharacter();
 	}
+
+	public void OnEnable()
+	{
+		InitCharacter();
+	}
+
+	public void InitCharacter()
+	{
+		theController = GetComponent<NpcMovement>();
+		parado = false;
+		patrolState = new PatrolState(this, theController);
+		chaseState = new ChaseState(this, theController);
+		controlledState = new ControlledState(this, theController);
+		diedState = new DiedState(this, theController);
+
+		//Life Initialitation
+		lifeBar = GetComponent<TankHealth>();
+		if (lifeBar != null) {
+			lifeBar.m_StartingHealth = vidaInicial;
+			lifeBar.DisableHealthBar ();
+		}
+
+		//Bool init
+		parasitado = false;
+		muerto = false;
+		canPatrol = true;
+
+		if(theController.anim != null)
+        {
+            theController.anim.SetBool("isDead", false);
+            theController.anim.Play("Iddle");
+        }
+			
+
+		currentState = patrolState;
+	}
+
 
 	void Start (){
 		currentState = patrolState;
@@ -40,6 +79,15 @@ public class StateEnemyBehavior : MonoBehaviour {
 	void Update (){
 		currentState.UpdateState ();
 		posPlayer = this.transform.position;
+	}
+
+	public void TakeDamage(float damage)
+	{
+		if(lifeBar != null && lifeBar.TakeDamage(damage)) //La vida ha llegado a 0
+		{
+			lifeBar.DisableHealthBar();
+			currentState.ToDiedState();
+		}
 	}
 
 	public void SightTriggered(Collider2D other) {
@@ -54,39 +102,40 @@ public class StateEnemyBehavior : MonoBehaviour {
 			currentState.ToPatrolState();
 	}
 
-    public void EnterControlZone(Collider2D other)
-    {
+	public void EnterControlZone(Collider2D other)
+	{
 		if(other.tag == "Player" && !muerto)
-        {
-            other.GetComponent<PlayerInput>().ToCanControl(theController);
-        }
-    }
+		{
+			other.GetComponent<PlayerInput>().ToCanControl(theController);
+		}
+	}
 
-    public void ControlZoneStay(Collider2D other)
-    {
+	public void ControlZoneStay(Collider2D other)
+	{
 		if(other.tag == "Player" && !muerto)
-        {
-            if(Input.GetKeyDown(KeyCode.E))
-            {
-				//Debug.Log ("Controlado");
+		{
+			if(Input.GetKeyDown(KeyCode.E))
+			{
+				controlledState.parasito = other.GetComponent<PlayerInput>();
+				controlledState.parasito.Parasitar();
 				canPatrol = false;
 				StartCoroutine (WaitForParasitar ());
-                other.GetComponent<PlayerInput>().Parasitar();
-                currentState.ToControlledState();
-            }
-        }
-    }
+				currentState.ToControlledState();
+			}
+		}
+	}
 
-    public void ExitControlZone(Collider2D other)
-    {
+	public void ExitControlZone(Collider2D other)
+	{
 		if(other.tag == "Player" && !muerto)
-        {
-            other.GetComponent<PlayerInput>().ExitControlZone();
-        }
-    }
+		{
+			other.GetComponent<PlayerInput>().ExitControlZone();
+		}
+	}
 
 	private IEnumerator WaitForParasitar(){
 		yield return new WaitForSeconds(1.3f);
+		lifeBar.EnableHealthBar();
 		parasitado = true;
 	}
 
@@ -102,5 +151,11 @@ public class StateEnemyBehavior : MonoBehaviour {
 		yield return new WaitForSeconds(4f);
 		parado = false;
 		this.GetComponent<NpcMovement> ().movementController.moveSpeed = 4;
+	}
+
+	private IEnumerator Respawn()
+	{
+		yield return new WaitForSeconds(3f);
+		this.gameObject.SetActive(false);
 	}
 }
